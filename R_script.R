13e4cf9e55489446e3878986fa415835f5672c79
brovoldinho <evengbro@hotmail.com>
2018-10-23 18:07:41 +0200

    Lest inn data på salmar og marine harvest, samt ryddet i tabell på laksepris

--- R_script.R
+++ R_script.R
@@ -5,6 +5,8 @@ library(rjstat)
 library(jsonlite)
 library(PxWebApiData)
 library(tidyr)
+library(readr)
+library(dplyr)
 
 laksepris <- ApiData("http://data.ssb.no/api/v0/dataset/1122.json?lang=no", 
                      getDataByGET = TRUE, col_types = cols(måned = col_date(format = "%Y%m")))
@@ -25,3 +27,31 @@ tail(laksepris)
 
 # en test for å se om det går og pushe
 # Ikke bra, men funka
+
+#Leser inn data fra netfonds på Salmar
+salmar <- read_csv("https://www.netfonds.no/quotes/paperhistory.php?paper=SALM.OSE&csv_format=csv")
+
+#Leser inn data fra netfonds på Marine harvest
+marine_harvest <- read_csv("https://www.netfonds.no/quotes/paperhistory.php?paper=MHG.OSE&csv_format=csv")
+
+# Ser at laksepris df er for både frossen og fersk laks. 
+# Vi burde årdne to kolonner med "kilospris frossen laks" og "kilospris fersk laks".
+# Samme gjelder vekt kolonnen. Unødvendig med en hel kolonne over varegruppe?
+# Prøver meg litt frem og ser om jeg får til dette. 
+# Om du får til mer oversiktlig kode på dette så bare erstatt den som er der!
+
+
+laksepris_fersk <- laksepris[1:980,1:4]
+
+laksepris_frosset <- laksepris[981:1960,1:4]
+
+laksepris <- bind_cols(laksepris_fersk, laksepris_frosset)
+
+
+#Endrer kolonnenavn
+colnames(laksepris) <- c("varegruppe", "date", "kilospris fersk laks", "vekt fersk laks", "varegruppe1", "date1", "kilospris frossen laks", "vekt frossen laks")
+
+#Fjerner unødvendige kolonner
+laksepris[c("varegruppe","varegruppe1", "date1")] <- list(NULL)
+
+rm(laksepris_fersk, laksepris_frosset)
